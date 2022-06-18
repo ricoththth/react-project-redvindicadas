ffcd2a611e6e8ce47e5c428206c044fe9ff5387c
Lizeth Rico <ricogomezlauralizeth@gmail.com>
2022-06-17 23:54:54 -0500

    finished me

--- src/components/stickers/Stickers.js
+++ src/components/stickers/Stickers.js
@@ -4,7 +4,7 @@ import Vectorkit1 from "../../icons/Vector kitfem1.png"
 import Vectorkit3 from "../../icons/Vector kitfem3.png"
 import Vectorkit2 from "../../icons/Vector kitfem2.png"
 import Vectorkit4 from "../../icons/Vector kitfem4.png"
-import {Card } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
 function Stickers(h) {
     return (
@@ -25,44 +25,86 @@ function Stickers(h) {
                 </div>
 
 
-                <div className='content-middle-cards'>
-                    <div className='card-stickers'>
-                        <div className='card-stickers1'>
-                        <img src="..." class="card-img-top stickers" alt="..."/>
+                <div className='content-middle-stickers'>
+                    <div class="card-sticker">
+                        <img src="https://ih1.redbubble.net/image.908013094.4393/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg" class="card-img-top stickers" alt="..."/>
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <h6 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h6>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <h5 class="card-title">Veo una mujer fuerte </h5>
+                                <h6 class="card-text">@evannave</h6>
+                                <a href="https://www.redbubble.com/es/i/pegatina/Veo-una-mujer-fuerte-de-evannave/25594393.EJUG5" Target="_blank" class="btn btn-primary">Comprar</a>
                             </div>
-                        </div>
-
-                        <div class="card-stickers1">
-                        <img src="..." class="card-img-top stickers" alt="..."/>
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <h6 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h6>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-
-                        <div class="card-stickers1">
-                        <img src="..." class="card-img-top stickers" alt="..."/>
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <h6 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h6>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-
-                        <div class="card-stickers1">
-                        <img src="..." class="card-img-top stickers" alt="..."/>
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <h6 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h6>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
                     </div>
+                    <div class="card-sticker">
+                    <img src="https://ih1.redbubble.net/image.1214799301.9266/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg" class="card-img-top stickers" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">La fuerza</h5>
+                                <h6 class="card-text">@Lorraine Rabbit</h6>
+                                <a href="https://www.redbubble.com/es/i/pegatina/La-fuerza-de-avindasart/48219266.EJUG5" Target="_blank" class="btn btn-primary">Comprar</a>
+                            </div>
+                    </div>
+                    <div class="card-sticker">
+                    <img src="https://ih1.redbubble.net/image.313609450.4668/st,small,507x507-pad,600x600,f8f8f8.u3.jpg" class="card-img-top stickers" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Boys Tears Juicebox </h5>
+                                <h6 class="card-text">@Royal Sass</h6>
+                                <a href="https://www.redbubble.com/es/i/pegatina/Boys-Tears-Juicebox-de-theroyalsass/24804668.EJUG5" Target="_blank" class="btn btn-primary">Comprar</a>
+                            </div>
+                    </div>
+
+                    <div class="card-sticker">
+                    <img src="https://ih1.redbubble.net/image.1513459722.2471/st,small,507x507-pad,600x600,f8f8f8.jpg" class="card-img-top stickers" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Como el pingüino rico </h5>
+                                <h6 class="card-text">@chnlrdt</h6>
+                                <a href="https://www.redbubble.com/es/i/pegatina/come-el-ping%C3%BCino-rico-de-chnlrdt/54492471.EJUG5" Target="_blank" class="btn btn-primary">Comprar</a>
+                            </div>
+                    </div>
+
+                    <div class="card-sticker">
+                    <img src="https://ih1.redbubble.net/image.118341382.8301/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg" class="card-img-top stickers" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Mis feministas amigas felinas. </h5>
+                                <h6 class="card-text">@tamaghosti</h6>
+                                <a href="https://www.redbubble.com/es/i/pegatina/Esta-imagen-fue-robada-VERSI%C3%93N-2-https-www-redbubble-com-shop-ap-49843578-asc-u-de-tamaghosti/11088301.EJUG5" Target="_blank" class="btn btn-primary">Comprar</a>
+                            </div>
+                    </div>
+
+                    <div class="card-sticker">
+                    <img src="https://ih1.redbubble.net/image.1826639420.6731/st,small,507x507-pad,600x600,f8f8f8.jpg" class="card-img-top stickers" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Las chicas odian el capitalismo. </h5>
+                                <h6 class="card-text">@pancakesandeggs</h6>
+                                <a href="https://www.redbubble.com/es/i/pegatina/las-chicas-odian-el-capitalismo-de-pancakesandeggs/61876731.EJUG5" Target="_blank" class="btn btn-primary">Comprar</a>
+                            </div>
+                    </div>
+
+                    <div class="card-sticker">
+                    <img src="https://ih1.redbubble.net/image.1083233763.1859/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg" class="card-img-top stickers" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">In Utero Graphic. </h5>
+                                <h6 class="card-text">@5040designs</h6>
+                                <a href="https://www.redbubble.com/es/i/pegatina/In-Utero-Graphic-de-5040designs/45281859.EJUG5" Target="_blank" class="btn btn-primary">Comprar</a>
+                            </div>
+                    </div>
+
+                    <div className='card-sticker'>
+                        <img src="https://ih1.redbubble.net/image.1835303426.4338/st,small,507x507-pad,600x600,f8f8f8.jpg" class="card-img-top stickers" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Mi cuerpo, mi decisión. </h5>
+                                <h6 class="card-text">@nydiesita</h6>
+                                <a href="https://www.redbubble.com/es/i/pegatina/Mi-cuerpo-mi-decisi%C3%B3n-de-nydiesita/62124788.EJUG5" Target="_blank" class="btn btn-primary">Comprar</a>
+                            </div>
+                    </div>
+
+                    <div className='card-stickerg'>
+                        <img src="https://ih1.redbubble.net/image.831627157.0990/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.u1.jpg" class="card-img-top stickers" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Aplastar el patriarcado. </h5>
+                                <h6 class="card-text">@Martina Scott</h6>
+                                <a href="" Target="_blank" class="btn btn-primary">Comprar</a>
+                            </div>
+                    </div>
+
                 </div>
 
 
